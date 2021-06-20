b150364d43f8a5c42dbfeb3a56ac8b9bd8e03395
Xerkzes <xerkzesavora@gmail.com>
2021-06-20 14:08:55 +0200

    added loading animation if the iframe is not loaded fully in yet

--- src/components/Sites/DraftLeague/DraftLeague.tsx
+++ src/components/Sites/DraftLeague/DraftLeague.tsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 interface DraftLeagueProps {}
 
 export const DraftLeague: React.FC<DraftLeagueProps> = ({}) => {
+  const [loadIframe, setLoadIfram] = useState<boolean>(true);
+
   return (
     <div>
       <h1>Draft League</h1>
+
+      {loadIframe && (
+        <div
+          className="pokemon-cards-loader"
+          style={{ display: loadIframe ? "block" : "none" }}
+        ></div>
+      )}
 
       <iframe
         src="https://challonge.com/ayr4jge7/module"
@@ -13,6 +23,8 @@ export const DraftLeague: React.FC<DraftLeagueProps> = ({}) => {
         height="600"
         frameBorder="0"
         scrolling="auto"
+        style={{ display: loadIframe ? "none" : "block" }}
+        onLoad={() => setLoadIfram(false)}
       ></iframe>
     </div>
   );
